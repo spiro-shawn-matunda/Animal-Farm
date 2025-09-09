c76991e33a07c2fd5ec69bb717b921dbadbbf7ad
Shawn  Matunda <shawn.matunda@spironet.com>
2025-09-09 14:52:19 +0300

    Initial commit

---
+++ src/test/java/com/spiro/animalfarm/AnimalfarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spiro.animalfarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalfarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
